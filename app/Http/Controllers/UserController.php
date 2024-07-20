07db8705daec0611d6a06ae5a74d57df6a37bbb3
Singgih Pratama <singgih.vendor.pasti@pegadaian.co.id>
2024-07-20 23:37:57 +0700

    development method Create, route for create and store, and make function create and store in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class UserController extends Controller
 {
     //
-    public function index(): \Inertia\Response
+    public function index(): Response
     {
 //        $users = User::all(); // menampilkan semua data
         $users = User::paginate(10);
@@ -17,5 +19,24 @@ class UserController extends Controller
         return Inertia::render('User/Index', [
             'users' => $users,
         ]);
+    }
+
+    public function create(): Response
+    {
+        return Inertia::render('User/Create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8',
+            'password_confirmation' => 'required|same:password',
+        ]);
+
+        User::create($request->all());
+
+        return redirect()->route('users');
     }
 }
